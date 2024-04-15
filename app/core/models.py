39522678033b363585e8f2934905d0e2c80b166a
bocheng24 <jeffcgp@hotmail.com>
2024-04-15 17:19:14 +0800

    create recipe model and app

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -36,3 +37,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'    # Over write the default USERNAME_FIELD value
+
+
+class Recipe(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,10 @@
 '''
 Test cases for models
 '''
+from decimal import Decimal
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
 
 
 class ModelTest(TestCase):
@@ -42,3 +44,19 @@ class ModelTest(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'pass123$',
+        )
+
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.34'),
+            description='Sample descriptions for recipe',
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
